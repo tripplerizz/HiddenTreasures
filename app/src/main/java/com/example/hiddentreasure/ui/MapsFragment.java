799752e7d8d61676b7f60c6ec22e6b9c556dfec4
Kevin Nguyen <nguyenkt6123@gmail.com>
2021-04-09 23:34:52 -0400

    added google maps fragment to treasure info fragment

--- app/src/main/java/com/example/hiddentreasure/ui/MapsFragment.java
+++ app/src/main/java/com/example/hiddentreasure/ui/MapsFragment.java
@@ -15,7 +15,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.CustomTarget;
@@ -32,20 +31,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.GeoPoint;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.model.Document;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MapsFragment extends Fragment {
     private static final String TAG = "MapsFragment";
@@ -60,24 +49,20 @@ public class MapsFragment extends Fragment {
                 ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
             }
             CollectionReference reference = mDatabase.getCollection();
+
+            // Enable the map to mark where you are with blue marker
             googleMap.setMyLocationEnabled(true);
+
+            // get current location of user
             Task<Location> task = mFusedLocationProviderClient.getLastLocation();
             task.addOnSuccessListener(location -> {
                 if (location != null) {
                     mCurrentLocation = location;
                     LatLng curLocation = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
-                    reference.addSnapshotListener((value, error) -> {
-                        for (DocumentSnapshot documentSnapshot : value.getDocuments()) {
-                            TreasureItem item = documentSnapshot.toObject(TreasureItem.class);
-                            GeoPoint itemLocation = item.getLocation();
-                            LatLng itemLatLng = new LatLng(itemLocation.getLatitude(), itemLocation.getLongitude());
-                            if (getActivity() == null)  {
-                                continue;
-                            }
-                            addMarker(googleMap, itemLatLng, item);
-                        }
-                    });
 
+                    addMarker(reference, googleMap);
+
+                    // move camera to current location
                     googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLocation, 14f));
                 }
             });
@@ -85,7 +70,24 @@ public class MapsFragment extends Fragment {
         }
     };
 
-    private void addMarker(GoogleMap googleMap, LatLng location, TreasureItem item) {
+    private void addMarker(CollectionReference reference, GoogleMap googleMap) {
+        reference.addSnapshotListener((value, error) -> {
+            // Iterate through each item in the database and add the item info to map
+            for (DocumentSnapshot documentSnapshot : value.getDocuments()) {
+                TreasureItem item = documentSnapshot.toObject(TreasureItem.class);
+                GeoPoint itemLocation = item.getLocation();
+                LatLng itemLatLng = new LatLng(itemLocation.getLatitude(), itemLocation.getLongitude());
+
+                if (getActivity() == null)  {
+                    continue;
+                }
+
+                addMarkerToMapWithPicture(googleMap, itemLatLng, item);
+            }
+        });
+    }
+
+    private void addMarkerToMapWithPicture(GoogleMap googleMap, LatLng location, TreasureItem item) {
         Glide.with(MapsFragment.this)
                 .asBitmap()
                 .load(item.getImageUrl())
@@ -112,8 +114,6 @@ public class MapsFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDatabase = TreasureDatabase.getInstance(getContext());
-
-
     }
 
     @Nullable
--- app/src/main/java/com/example/hiddentreasure/ui/TreasureInfoFragment.java
+++ app/src/main/java/com/example/hiddentreasure/ui/TreasureInfoFragment.java
@@ -1,9 +1,14 @@
 package com.example.hiddentreasure.ui;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -16,11 +21,24 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.hiddentreasure.R;
 import com.example.hiddentreasure.db.TreasureItem;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.GeoPoint;
 
 public class TreasureInfoFragment extends Fragment {
     private static final String TAG = "TreasureInfoFragment";
     private TreasureItem item;
     private String itemInfoString = "Name: %s\nDescription: %s\n";
+    private SupportMapFragment mGoogleMap;
+    private TextView mDescription;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,16 +46,21 @@ public class TreasureInfoFragment extends Fragment {
         Bundle bundle = getArguments();
         if (bundle != null) {
             item = bundle.getParcelable(HomeFragment.TREASURE_TAG);
-            Log.d(TAG, "onCreate: " + item.getName());
-            Log.d(TAG, "onCreate: " + item.getDescription());
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_treasure_info, container, false);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+
         ImageView itemImage = v.findViewById(R.id.itemImage);
         String ImageUrl = item.getImageUrl();
         Glide
@@ -46,8 +69,33 @@ public class TreasureInfoFragment extends Fragment {
                 .centerCrop()
                 .into(itemImage);
         TextView itemInfo = v.findViewById(R.id.itemInfo);
-        itemInfoString = String.format(itemInfoString, item.getName(), item.getDescription());
-        itemInfo.setText(itemInfoString);
+        mDescription = v.findViewById(R.id.description_tv);
+        itemInfo.setText(item.getName());
+        mDescription.setText(item.getDescription());
         return v;
     }
+
+    private final OnMapReadyCallback callback = googleMap -> {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        GeoPoint geoPoint = item.getLocation();
+        LatLng location = new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude());
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(location)
+                .title(item.getName())
+        );
+
+        googleMap.setOnMarkerClickListener((Marker marker) -> {
+            LatLng position = marker.getPosition();
+            Uri uri = Uri.parse("geo:0,0?q=" + position.latitude + "," + position.longitude);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+            return true;
+        });
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 14f));
+    };
 }
