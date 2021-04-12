8fd6d0dc78cae330258d449f6c03fae678c6b233
tripplerizz <hectormrizo@gmail.com>
2021-04-12 12:37:20 -0400

    added stack function and skipButton function to Pirate Addies

--- app/src/main/java/com/example/hiddentreasure/ui/PirateSwipeFragment.java
+++ app/src/main/java/com/example/hiddentreasure/ui/PirateSwipeFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.ImageViewTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.example.hiddentreasure.R;
 import com.example.hiddentreasure.db.TreasureDatabase;
@@ -34,15 +36,16 @@ import java.util.Stack;
 public class PirateSwipeFragment extends Fragment {
     private Stack<TreasureItem> items = new Stack<TreasureItem>();
     private TreasureDatabase db;
+    private  CollectionReference collectionReference;
+    private  ImageView imageView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // populating list with treasure items
         db = TreasureDatabase.getInstance(getContext());
-        CollectionReference collectionReference = db.getCollection();
-        findTreasureItem(collectionReference);
+        collectionReference = db.getCollection();
     }
-    private void findTreasureItem(CollectionReference reference) {
+    private void findTreasureItem(CollectionReference reference, View v) {
         reference.addSnapshotListener((value, error) -> {
             // Iterate through each item in the database and add the item info to map
             for (DocumentSnapshot documentSnapshot : value.getDocuments()) {
@@ -52,6 +55,7 @@ public class PirateSwipeFragment extends Fragment {
                     continue;
                 }
             }
+            readyTreasureCardView(v);
         });
     }
 
@@ -59,18 +63,29 @@ public class PirateSwipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.pirate_treasure_tinder, container, false);
-        readyTreasureCardView(v);
-
+        findTreasureItem(collectionReference, v);
+        // Skipping items which were unvisited by Pirate
+        ImageButton skipButton = v.findViewById(R.id.skipButton);
+        skipButton.setOnClickListener(view -> {
+            items.pop();
+            readyTreasureCardView(v);
+        });
+        ImageButton acceptButton = v.findViewById(R.id.acceptButton);
         return v;
     }
     public void readyTreasureCardView(View v){
-        ImageView imageView = v.findViewById(R.id.ItemSwipePhoto);
+        imageView = v.findViewById(R.id.ItemSwipePhoto);
         String ImageUrl = items.peek().getImageUrl();
         Glide
                 .with(imageView.getContext())
                 .load(ImageUrl)
                 .centerCrop()
-                .into(imageView);
+                .into(new ImageViewTarget<Drawable>(imageView) {
+                    @Override
+                    protected void setResource(@Nullable Drawable resource) {
+                        imageView.setImageDrawable(resource);
+                    }
+                });
 
     }
 
